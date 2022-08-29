e0d8a73aedbe41dbf8fe678eef6a721f9d8f9d4a
Deryn Brown <Deryn.2017@gmail.com>
2022-08-29 18:23:47 -0500

    commit tree e05858dc7bec87ee8f95cadc9b00e7491beedece

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42);
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(endBlock - startBlock) * 264;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    let currentDistance = distanceTravelledInFeet(startBlock, endBlock);
+    let farePrice = 0;
+    if (currentDistance < 400) {
+        return farePrice / 100;
+    } else if (currentDistance >= 400 && currentDistance <= 2000) {
+        return (currentDistance - 400) * 2 / 100;
+    } else if (currentDistance > 2000 && currentDistance < 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
